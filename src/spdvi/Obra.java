f490382b5e8118f16a8809d0ee7beed67eeceb85
spdvi <gjm@paucasesnovescifp.cat>
2021-10-30 14:40:20 +0200

    Read obras information from the json file and show it in a JList.

---
+++ src/spdvi/Obra.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spdvi;
+
+/**
+ *
+ * @author DevMike
+ */
+public class Obra {
+        public String getRegistre() {
+        return registre;
+    }
+
+    public void setRegistre(String registre) {
+        this.registre = registre;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public String getAny() {
+        return any;
+    }
+
+    public void setAny(String any) {
+        this.any = any;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    private String registre;
+    private String titol;
+    private String any;
+    private String format;
+    private String autor;
+
+    @Override
+    public String toString() {
+        return registre + ": " + titol + ", " + any + " (" + format + "). " + autor; 
+    }
+}
